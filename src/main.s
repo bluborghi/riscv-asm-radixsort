cc692045f801c5003a0a7d4f82bead85ef6ed7cf
Blu Borghi <blu.borghi@gmail.com>
2019-08-31 13:20:24 +0200

    last changes

--- src/main.s
+++ src/main.s
@@ -5,6 +5,12 @@
 .section .data
     arr: .word 170, 45, 75, 90, 802, 69, 4, 20
     n: .word 8
+    #arr: .word 423,65,1004,53,5
+    #n: .word 5
+    #arr: .word 2, 90, 20, 1
+    #n: .word 4
+    #arr: .word 24985, 399824, 298342
+    #n: .word 3
 
 
 
